a953ad99b8a7af5564eaccec94ba21272aaae1f6
carbonicality <carbonical80@gmail.com>
2025-11-15 16:10:28 +0000

    make it opaque

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('loginTime').textContent = new Date().toLocaleString();
+let credits = 0;
+let clickPwr = 1;
+let cps = 0;
+let startTime = Date.now();
+let solar = 0, fusion = 0, siege = 0, quantum = 0;
